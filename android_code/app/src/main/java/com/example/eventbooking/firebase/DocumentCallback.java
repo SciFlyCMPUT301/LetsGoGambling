37374a46670cd0463bc387eb707a5e302f85d422
Alex Flewwelling <alex.flewwelling@gmail.com>
2024-10-29 16:53:42 -0600

    Delete DocumentCallback.java

--- android_code/app/src/main/java/com/example/eventbooking/firebase/DocumentCallback.java
+++
@@ -1,7 +0,0 @@
-package com.example.eventbooking.firebase;
-
-import com.google.firebase.firestore.DocumentSnapshot;
-
-public interface DocumentCallback {
-    void onCallback(DocumentSnapshot snapshot);
-}

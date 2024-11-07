a13ec391ac2b89680ea001729f92ec1a0aad8859
heily <chhatiaw@ualberta.ca>
2024-11-07 14:37:11 -0700

    comments

--- android_code/app/src/main/java/com/example/eventbooking/Login/CreateUserFragment.java
+++ android_code/app/src/main/java/com/example/eventbooking/Login/CreateUserFragment.java
@@ -9,6 +9,9 @@ import androidx.fragment.app.Fragment;
 
 import com.example.eventbooking.R;
 
+/**
+ * A placeholder fragment containing a simple view.
+ */
 public class CreateUserFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
